42fbf907e3d29b370b1b073d96fe5a8242a850b5
Wayne Choi <waynechoi@gmail.com>
2023-06-30 20:27:14 -0700

    Added random access

--- ddl.sql
+++ ddl.sql
@@ -2,5 +2,8 @@ CREATE TABLE reviews (
     album text,
     artist text,
     rating real,
+    release_year integer,
+    genre text,
+    review_date DATETIME,
     inserted_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
